40dedaa9117095850e5fd8f2d72d9f6801f395fd
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-02-11 22:00:59 +0100

    Started working with shopping cart functionality

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+const addToCartElement = document.getElementByClass("add-to-cart");
+const itemNameElement = document.getElementById("item-name");
+const buttonPoloRalphLaurenKnittedSweaterBlack = document.getElementById(
+  "button-polo-ralph-lauren-knitted-sweater-black"
+);
+const buttonPoloRalphLaurenFullZipCardiganFawnGreyHeather =
+  document.getElementById(
+    "button-polo-ralph-lauren-full-zip-cardigan-fawn-grey-heather"
+  );
+
+addToCartElement.addEventListener("click", function (e) {
+    let clothingItemName = 
+    
+});
+
+/*
+It should be possible to add two different products to the cart, and they should remain there regardless of which page you navigate to. You must use either localStorage or sessionStorage for this.
+
+It should be possible to remove products from the cart. You must use either localStorage or sessionStorage for this.
+
+There should be at least one animation with a minimum of 2 keyframes.
+*/
