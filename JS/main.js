df0e2cb74a7c8249b34bbf7e12975ff98ed4a63f
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-02-12 13:46:17 +0100

    Made it possible to add the two first sweaters into the session storage
    Have not managed to make the text appear in the shopping cart yet

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,43 @@
-const addToCartElement = document.getElementByClass("add-to-cart");
-const itemNameElement = document.getElementById("item-name");
-const buttonPoloRalphLaurenKnittedSweaterBlack = document.getElementById(
+const shoppingCart = document.getElementById("shopping-cart");
+const liElement = document.createElement("li");
+const itemNameSweaterOne = document.getElementById("item-name-sweater-one");
+const itemNameSweaterTwo = document.getElementById("item-name-sweater-two");
+
+const sweaterOneButton = document.getElementById(
   "button-polo-ralph-lauren-knitted-sweater-black"
 );
-const buttonPoloRalphLaurenFullZipCardiganFawnGreyHeather =
-  document.getElementById(
-    "button-polo-ralph-lauren-full-zip-cardigan-fawn-grey-heather"
-  );
+const sweaterTwoButton = document.getElementById(
+  "button-polo-ralph-lauren-full-zip-cardigan-fawn-grey-heather"
+);
 
-addToCartElement.addEventListener("click", function (e) {
-    let clothingItemName = 
-    
+sweaterOneButton.addEventListener("click", function (event) {
+  let clothing = "itemNameSweaterOne.innerText";
+
+  sessionStorage.sweaterOne = clothing;
+
+  let newClothing = sessionStorage.clothing;
+
+  liElement.innerText = newClothing;
+
+  shoppingCart.appendChild(liElement);
+});
+
+sweaterTwoButton.addEventListener("click", function (event) {
+  let clothing = itemNameSweaterTwo.innerText;
+
+  sessionStorage.sweaterTwo = clothing;
+
+  let newClothing = sessionStorage.clothing;
+
+  liElement.innerText = newClothing;
+
+  shoppingCart.appendChild(liElement);
 });
 
 /*
-It should be possible to add two different products to the cart, and they should remain there regardless of which page you navigate to. You must use either localStorage or sessionStorage for this.
+It should be possible to add two different products to the cart, and they should remain there regardless of which page you navigate to. You must use either sessionStorage or sessionStorage for this.
 
-It should be possible to remove products from the cart. You must use either localStorage or sessionStorage for this.
+It should be possible to remove products from the cart. You must use either sessionStorage or sessionStorage for this.
 
 There should be at least one animation with a minimum of 2 keyframes.
 */
