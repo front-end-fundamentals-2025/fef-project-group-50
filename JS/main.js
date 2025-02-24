518928730242c8af9a966d305c4e52daddc63d4c
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-02-24 17:40:59 +0100

    Removed a comment

--- JS/main.js
+++ JS/main.js
@@ -16,10 +16,3 @@ sweaterTwoButton.addEventListener("click", function () {
   let clothing = itemNameSweaterTwo.innerText;
   sessionStorage.sweaterTwo = clothing;
 });
-
-/*
-It should be possible to add two different products to the cart, and they should remain there regardless of which page you navigate to. 
-You must use either localStorage  or sessionStorage for this.
-It should be possible to remove products from the cart. You must use either localStorage or sessionStorage for this.
-There should be at least one animation with a minimum of 2 keyframes.
-*/
